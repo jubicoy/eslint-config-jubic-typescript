9949f48ba232d71973a6855e774c4f02b3dabf99
Aleksi Gold <aleksi.gold@jubic.fi>
2020-10-12 13:25:13 +0300

    Fix extends ordering

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   'extends': [
-    'plugin:@typescript-eslint/recommended',
-    'jubic'
+    'jubic',
+    'plugin:@typescript-eslint/recommended'
   ],
   'parser': '@typescript-eslint/parser',
   'plugins': ['@typescript-eslint'],
