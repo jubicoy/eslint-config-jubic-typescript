c1d5b1be38835d14414be2a4e0a0b9e8c2d1974c
Vilppu Vuorinen <vilppu.vuorinen@jubic.fi>
2021-05-06 11:38:53 +0300

    Use typescript no-use-before-define rule

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ module.exports = {
     '@typescript-eslint/indent': ['error', 2],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
-    "semi": "off",
-    "@typescript-eslint/semi": ["error"]
+    'semi': 'off',
+    '@typescript-eslint/semi': ['error'],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error']
   }
 }
